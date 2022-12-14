a1f3b077295b76a5419c45514e2fd7f6a34138b8
Marc Gavanier <marc.gavanier@gmail.com>
2022-12-14 16:42:41 +0100

    fix: remove service prefix from workspace name output

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "workspace_name" {
-  value       = tfe_workspace.ephemeral_environment.name
-  description = "The name of the terraform workspace for the branch ephemeral environment"
+  value       = "feature-${local.workspace_name}"
+  description = "The name of the terraform workspace for the branch ephemeral environment without service prefix"
 }
